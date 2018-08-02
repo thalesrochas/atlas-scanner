d866ccf40c6af493cf63130b6b2d034448c6094f
Thales Rocha <thales_rochasousa@hotmail.com>
2018-08-02 16:24:21 -0300

    Adicionado motor com encoder. Interrupção do encoder configurada. Armazenamento de coordenadas no cartão.

--- Arduino/Atlas/Atlas.ino
+++ Arduino/Atlas/Atlas.ino
@@ -4,14 +4,34 @@
 #include <SPI.h>
 #include <SD.h>
 
-//* Define a porta em que o endstop do motor de passo está ligada.
-byte ENDSTOP = 4;
+//* Define o pino do motor.
+const byte MOTOR_PIN = 2;
+
+//* Define o pino do encoder do motor.
+const byte ENCODER_PIN = 3;
+
+//* Define o pino do endstop do motor.
+const byte ENDSTOP_PIN = 4;
 
 //* Define o pino CS do leitor de cartão.
-byte CS_PIN = 10;
+const byte CS_PIN = 10;
 
 // Define a quantidade máxima de passos que o motor pode girar.
-int STEP_LIMIT = 355;
+const int STEP_LIMIT = 355;
+
+/**
+ * Define a quantidade de ticks do encoder por volta.
+ * Valor aproximado obtido através de testes.
+ * Necessita fazer uma compensação via código para alinhas os
+ * pontos a cada volta.
+ */
+const int TICKS_PER_TURN = 2245;
+
+/**
+ * Armazena quantos ticks já ocorreram na volta atual.
+ * Utilizado no cálculo do theta.
+ */
+int ticks = 0;
 
 /**
  * * Conexões do Leitor de Cartão:
@@ -45,7 +65,7 @@ LIDARLite lidar;
 Stepper stepper = Stepper(400, 6, 7, 8, 9);
 
 void setup() {
-    Serial.begin(38400);
+    Serial.begin(38400); // Inicializa a porta Serial.
     Serial.println("Porta Serial inicializada.\n");
 
     Serial.println("Inicializando cartão SD...");
@@ -56,6 +76,23 @@ void setup() {
     else {
         Serial.println("Cartão SD Inicializado.\n");
     }
+
+    /**
+     * Configuração do motor.
+     * Inicializa o pino como saída.
+     * Seta a saída como desligada.
+     */
+    pinMode(MOTOR_PIN, OUTPUT);
+    digitalWrite(MOTOR_PIN, LOW);
+
+    /**
+     * Inicializa um pino de interrupção para o Encoder do motor.
+     * A interrupção acontece toda vez que o estado do pino 3 muda -> CHANGE.
+     * Cada vez que uma interrupção acontecer, é disparada a função interrupt.
+     */
+    pinMode(ENCODER_PIN, INPUT); // Define como entrada
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), interrupt, CHANGE);
+    //? detachInterrupt(digitalPinToInterrupt(ENCODER_PIN));
 
     /**
      * O método begin inicializa e define a configuração do sensor.
@@ -82,12 +119,12 @@ void setup() {
     lidar.begin(0, true);
     Serial.println("Sensor LIDAR inicializado.\n");
 
-    pinMode(ENDSTOP, INPUT); // Define o endstop como entrada
+    pinMode(ENDSTOP_PIN, INPUT); // Define o endstop como entrada
     stepper.setSpeed(80); // Configura o motor para velocidade de 80rpm.
 
     // Leva o suporte ao início até o endstop ser ativado.
     //? Há uma negação (!) na expressão devido a lógica invertida do botão.
-    while (!digitalRead(ENDSTOP)) {
+    while (!digitalRead(ENDSTOP_PIN)) {
         // O sinal negativo representa a direção contrária do giro.
         stepper.step(-1);
     }
@@ -96,6 +133,7 @@ void setup() {
 }
 
 void loop() {
+    Serial.println("Nova medição");
     // Recebe pela porta serial o nome do arquivo de pontos.
     String filename = "";
     Serial.print("Filename: ");
@@ -114,10 +152,20 @@ void loop() {
         while(true);
     }
     
+    Serial.println("Iniciando motor...");
+    digitalWrite(MOTOR_PIN, HIGH);
+    delay(2000); // Delay para aguardar que o motor pegue velocidade.
+
     Serial.println("Iniciando captura de pontos...");
 
-    // Captura as distâncias e insere os dados no arquivo
-    int distancia = 0;
+    /**
+     * Captura as distâncias em coordenadas esféricas e insere os
+     * dados no arquivo.
+     *      0 <=r < inf
+     *      0 <= theta < 360 (2pi)
+     *      0 <= phi < 180 (pi)
+     */
+
     /**
      * O método distance dispara o sensor e realiza uma medição.
      * 
@@ -126,16 +174,36 @@ void loop() {
      *      true: Medição com a correção de polarização. Deve ser realizada
      *            periodicamente (1 em cada 100 medições). -> Default
      */
-    distancia = lidar.distance();
-    //// Serial.println(distancia);
-    file.println(distancia);
+    float r = lidar.distance();
+    float theta = ticks * (360 / (TICKS_PER_TURN - 1)); // Regra de 3
+    // TODO: Calcular valor de phi através da posição do motor de passo.
+    float phi = 180;
 
-    for(byte i = 0; i < 99; i++) {
-        distancia = lidar.distance(false);
-        //// Serial.println(distancia);
-        file.println(distancia);
+    file.print(distancia);
+    file.print("\t");
+    file.print(theta);
+    file.print("\t");
+    file.println(phi);
+
+    for(byte i = 0; i < 100; i++) {
+        r = lidar.distance(false);
+        theta = ticks * (360 / (TICKS_PER_TURN - 1)); // Regra de 3
+        
+        file.print(distancia);
+        file.print("\t");
+        file.print(theta);
+        file.print("\t");
+        file.println(phi);
     }
 
     file.close(); // Fecha o acesso ao arquivo
-    Serial.println("Fim da captura de pontos.");
+    Serial.println("Fim da captura de pontos.\n");
+
+    Serial.println("Motor desligado.\n");
+    digitalWrite(MOTOR_PIN, LOW);
+}
+
+void interrupt() {
+    ticks++;
+    ticks %= TICKS_PER_TURN;
 }
