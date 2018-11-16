e1227a952c7779500be457e4fde93ac341b4c6a6
Thales Rocha <thales_rochasousa@hotmail.com>
2018-11-15 23:52:35 -0300

    Adicionados arquivos de teste

---
+++ Arquivos de Testes/13.09.18/delay100.m
@@ -0,0 +1,14 @@
+pontos = importdata('TESTEDELAY100.TXT');
+pontos1 = pontos(84:336, 1:2);
+pontos2 = pontos(337:500, 1:2);
+
+theta1 = pontos1(:,2);
+rho1 = pontos1(:,1);
+polar(deg2rad(theta1), rho1);
+
+hold on;
+
+theta2 = pontos2(:,2);
+rho2 = pontos2(:,1);
+% theta2 = theta2 - 16;
+polar(deg2rad(theta2), rho2);
--- Arquivos de Testes/28.09.18/test.m
+++ Arquivos de Testes/28.09.18/test.m
@@ -1,7 +1,7 @@
-pontos = importdata('OBJETO.TXT');
-pontos1 = pontos(72:300, 1:2);
-pontos2 = pontos(301:523, 1:2);
-pontos3 = pontos(524:758, 1:2);
+pontos = importdata('01.TXT');
+pontos1 = pontos(195:483, 1:2);
+pontos2 = pontos(484:770, 1:2);
+pontos3 = pontos(771:1065, 1:2);
 
 theta1 = pontos1(:,2);
 rho1 = pontos1(:,1);
@@ -11,10 +11,10 @@ hold on;
 
 theta2 = pontos2(:,2);
 rho2 = pontos2(:,1);
-% theta2 = theta2 - 16;
+theta2 = theta2 - 16;
 polar(deg2rad(theta2), rho2);
 
 % theta3 = pontos3(:,2);
 % rho3 = pontos3(:,1);
-% % theta3 = theta3 - 32;
+% %theta3 = theta3 - 32;
 % polar(deg2rad(theta3), rho3);
