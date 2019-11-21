8ffb5885f80718769abe80982aa252495d4e877c
Thales Rocha <thales_rochasousa@hotmail.com>
2019-11-21 01:21:40 -0300

    Adicionados resultados que serão usados no TCC

---
+++ Arquivos de Testes/TCC/pointcloud.m
@@ -0,0 +1,16 @@
+clear
+clc
+% close all
+
+pontos = importdata('CHAOF.TXT');
+% pontos = pontos(1:17000, :);
+
+rho = pontos(:,1);
+theta = deg2rad(pontos(:,2) - 15);
+phi = deg2rad(pontos(:,3));
+
+x = rho .* sin(phi) .* cos(theta);
+y = rho .* sin(phi) .* sin(theta);
+z = rho .* cos(phi);
+
+plot3(x, y, z, '.');
